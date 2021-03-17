74306ee6d7841557a6c5641a825cc9873836017f
Milind Jamnekar <milindjamnekar502@gmail.com>
2021-03-17 11:14:07 +0530

    SearchParams module created with hooks

---
+++ src/SearchParams.js
@@ -0,0 +1,22 @@
+import { useState } from "react";
+
+const SearchParams = () => {
+  const [location, setLocation] = useState("Seattle, WA");
+
+  return (
+    <div className="search-params">
+      <form>
+        <label htmlFor="location">location</label>
+        <input
+          id="location"
+          onChange={e => setLocation(e.target.value)}
+          value={location}
+          placeholder="Location"
+        ></input>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default SearchParams;
